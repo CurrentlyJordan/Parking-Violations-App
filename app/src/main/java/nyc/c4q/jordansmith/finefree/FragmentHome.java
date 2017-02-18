e18fbcf620176b074a182a6222e95cebe6668e21
cito125 <cito125@gmail.com>
2017-02-18 17:44:43 -0500

    removed a few souts

--- app/src/main/java/nyc/c4q/jordansmith/finefree/FragmentHome.java
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/FragmentHome.java
@@ -53,7 +53,6 @@ public class FragmentHome extends Fragment {
                     @Override
                     public void onResponse(Call<List<ParkingCameraResponse>> call, Response<List<ParkingCameraResponse>> response) {
                         List<ParkingCameraResponse> violationsList = parseResponseForOutstandingViolations(response.body());
-                        System.out.println(call.request().url());
                         mViolationsAdapter.setViolationsList(violationsList);
                     }
 
@@ -69,7 +68,6 @@ public class FragmentHome extends Fragment {
         List<ParkingCameraResponse> responseViolations = new ArrayList<>();
         for (int i = 0; i < body.size(); i++) {
             ParkingCameraResponse response = body.get(i);
-            System.out.println(Integer.valueOf(response.getAmountDue()));
             if(response.getAmountDue() != 0){
                 responseViolations.add(response);
             }
