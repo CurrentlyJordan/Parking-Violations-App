c655a8468f19732f14345bd7a96fbd307cfda0ec
CurrentlyJordan <jwsmith451@gmail.com>
2017-02-18 12:33:14 -0500

    more merge conflicts

--- app/src/main/java/nyc/c4q/jordansmith/finefree/MainActivity.java
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/MainActivity.java
@@ -1,16 +1,7 @@
 package nyc.c4q.jordansmith.finefree;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.List;
-
-import nyc.c4q.jordansmith.finefree.model.ParkingCameraResponse;
-import nyc.c4q.jordansmith.finefree.network.parking_camera_violations.ParkingCameraViolationsClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(getApplicationContext(), ViolationsActivity.class);
-        startActivity(intent);
+        
     }
 
 
