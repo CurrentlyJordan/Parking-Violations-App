69918a1c48c7db5f2cd1268a6ab5b3c6ef19df39
CurrentlyJordan <jwsmith451@gmail.com>
2017-02-18 12:02:51 -0500

    added navigation drawer

---
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/HomeFragment.java
@@ -0,0 +1,25 @@
+package nyc.c4q.jordansmith.finefree;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jordansmith on 2/18/17.
+ */
+
+public class HomeFragment extends Fragment {
+
+    View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.home_fragment_layout, container, false);
+
+        return rootView;
+    }
+}
