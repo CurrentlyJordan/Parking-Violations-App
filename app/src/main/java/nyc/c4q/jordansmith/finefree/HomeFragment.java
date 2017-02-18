14186e2e61a63bd22fb88c1aff7bdf98bb54c88a
Helen Chan <hchan22@gmail.com>
2017-02-18 17:01:46 -0500

    violation recyclerview

--- app/src/main/java/nyc/c4q/jordansmith/finefree/HomeFragment.java
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/HomeFragment.java
@@ -1,25 +1,68 @@
 package nyc.c4q.jordansmith.finefree;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nyc.c4q.jordansmith.finefree.model.ParkingCameraResponse;
+import nyc.c4q.jordansmith.finefree.network.parking_camera_violations.ParkingCameraViolationsClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by jordansmith on 2/18/17.
  */
 
 public class HomeFragment extends Fragment {
-
+    private RecyclerView violationRV;
+    private SharedPreferences preferences;
+    List<ParkingCameraResponse> violationsList = new ArrayList<>();
+    String licensePlate = "GXE1257";
     View rootView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.home_fragment_layout, container, false);
-
+        violationRV = (RecyclerView) rootView.findViewById(R.id.violations_recyclerview);
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fetchViolations();
+    }
+
+    private void fetchViolations() {
+        ParkingCameraViolationsClient
+                .getInstance()
+                .getResponseByPlate(licensePlate)
+                .enqueue(new Callback<List<ParkingCameraResponse>>() {
+                    @Override
+                    public void onResponse(Call<List<ParkingCameraResponse>> call, Response<List<ParkingCameraResponse>> response) {
+                        violationsList = response.body();
+                        violationRV.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+                        violationRV.setAdapter(new ViolationsAdapter(violationsList));
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<ParkingCameraResponse>> call, Throwable t) {
+                        Toast.makeText(rootView.getContext(), "Unable to Download Data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        ;
     }
 }
---
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/ViolationViewHolder.java
@@ -0,0 +1,26 @@
+package nyc.c4q.jordansmith.finefree;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import nyc.c4q.jordansmith.finefree.model.ParkingCameraResponse;
+
+/**
+ * Created by helenchan on 2/18/17.
+ */
+public class ViolationViewHolder extends RecyclerView.ViewHolder {
+    TextView summons_tv;
+    TextView fineAmount;
+
+    public ViolationViewHolder(View itemView) {
+        super(itemView);
+        summons_tv = (TextView) itemView.findViewById(R.id.summons_textview);
+        fineAmount = (TextView) itemView.findViewById(R.id.fine_textview);
+    }
+
+    public void bind(ParkingCameraResponse violations) {
+        summons_tv.setText(violations.getSummonsNumber());
+        fineAmount.setText(violations.getPaymentAmount());
+    }
+}
