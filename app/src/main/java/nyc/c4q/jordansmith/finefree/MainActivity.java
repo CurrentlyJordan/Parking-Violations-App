c305ff92b4f34c6dafde2d45361ebd71ddfa12de
cito125 <cito125@gmail.com>
2017-02-18 12:01:54 -0500

    added parking service client

--- app/src/main/java/nyc/c4q/jordansmith/finefree/MainActivity.java
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/MainActivity.java
@@ -3,6 +3,14 @@ package nyc.c4q.jordansmith.finefree;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import nyc.c4q.jordansmith.finefree.model.ParkingCameraResponse;
+import nyc.c4q.jordansmith.finefree.network.parking_camera_violations.ParkingCameraViolationsClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
