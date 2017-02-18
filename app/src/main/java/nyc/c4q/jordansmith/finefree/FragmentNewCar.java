ae8b0bf5c41ba45f6b0a3d5f8e4dec94fefd95bd
CurrentlyJordan <jwsmith451@gmail.com>
2017-02-18 15:33:18 -0500

    adding new car to nav drawer

--- app/src/main/java/nyc/c4q/jordansmith/finefree/FragmentNewCar.java
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/FragmentNewCar.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by jordansmith on 2/18/17.
@@ -13,14 +15,33 @@ import android.view.ViewGroup;
 
 public class FragmentNewCar extends Fragment {
     View rootView;
+    EditText newCarLicenseEditText;
+    EditText newCarNameEditText;
+    Button submitButton;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.new_car_fragment_layout, container, false);
+        newCarLicenseEditText = (EditText) rootView.findViewById(R.id.new_car_licence_edit_text);
+        newCarNameEditText = (EditText) rootView.findViewById(R.id.new_car_name_edit_text);
+        submitButton = (Button) rootView.findViewById(R.id.submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String savedPlate = newCarLicenseEditText.getText().toString();
+                String savedCarName = newCarNameEditText.getText().toString();
+
+
+            }
+        });
 
         return rootView;
     }
+
+
+
 
 }
