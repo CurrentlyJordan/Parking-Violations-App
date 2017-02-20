1c60c6f6baf056f1afbf357344a15effffc67585
CurrentlyJordan <jwsmith451@gmail.com>
2017-02-19 19:10:19 -0500

    adding settings and other things

---
+++ app/src/main/java/nyc/c4q/jordansmith/finefree/network/new_york_311/NewYork311Client.java
@@ -0,0 +1,10 @@
+package nyc.c4q.jordansmith.finefree.network.new_york_311;
+
+/**
+ * Created by jordansmith on 2/19/17.
+ */
+
+public class NewYork311Client {
+
+    private final String BASE_URL = 
+}
